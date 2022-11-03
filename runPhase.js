dd74e4b63ecc8dad5050462bf6d3191c062c87ba
aviehu <aviehu@gmail.com>
2022-11-03 12:47:59 +0200

    added ack loop to wait for the motors to stop moving

--- runPhase.js
+++ runPhase.js
@@ -1,18 +1,19 @@
 import {readFile} from "./fileHandler.js";
 import positionToPercentage from './positionToPercentage.json' assert {type: 'json'};
-import sendOsc from "./sendOsc.js";
+import sendMessages from "./sendOsc.js";
+
 
 export default function runPhase(phase) {
     readFile(phase, async (phaseFile) => {
         const phaseJson = JSON.parse(phaseFile)
-        for(const movement of phaseJson ) {
+        const messageQueue = phaseJson.map((movement) => {
             const { position } = movement
-            const message = {
+            return {
                 ...positionToPercentage.filter((ptp) => ptp.position === parseInt(position))[0],
                 ...movement
             }
-            await runMovement(message)
-        }
+        })
+        sendMessages(messageQueue)
     })
 }
 
@@ -20,7 +21,8 @@ function runMovement(message) {
     const { timeToGet, timeToStay } = message
     const waitingTime = parseInt(timeToGet) * 1000 + parseInt(timeToStay) * 1000
     return new Promise((resolve) => {
-        //sendOsc(message)
+        sendOsc(message)
+        console.log(message)
         setTimeout(() => {
             resolve(null)
         }, waitingTime)
--- sendOsc.js
+++ sendOsc.js
@@ -2,8 +2,8 @@ import osc from 'osc'
 
 const oscPort = new osc.UDPPort({
     remotePort: 8888,
-    remoteAddress:"10.0.0.1",
-    localAddress: "0.0.0.0",
+    remoteAddress:"127.0.0.1",
+    localPort: 4444
 });
 
 oscPort.open();
@@ -15,30 +15,60 @@ oscPort.on("error", function (error) {
     console.log("An error occurred: ", error.message);
 });
 
-export default function sendOsc(message) {
-    const { leftHand, rightHand, leftLeg, rightLeg, timeToGet } = message
+const messageQueue = []
+const timeoutQueue = []
+
+oscPort.on("message", (oscMsg, timeTag, info) => {
+    console.log("Done")
+    if(oscMsg.address === '/ack' && messageQueue.length > 0) {
+        setTimeout(() => {
+            sendOsc(messageQueue.shift())
+        }, timeoutQueue.shift() * 1000)
+    } else {
+        messageQueue.length = 0
+        timeoutQueue.length = 0
+        console.log("Phase Done")
+    }
+})
+
+export default function sendMessages(messages) {
+    if(messageQueue.length !== 0) {
+        console.log(`Start phase ignored - tried to start another phase while a phase is running`)
+        return
+    }
+    for(const message of messages) {
+        messageQueue.push(message)
+        timeoutQueue.push(message.timeToStay)
+    }
+    const firstMessage = messageQueue.shift()
+    sendOsc(firstMessage)
+}
+
+function sendOsc(message) {
+    const { leftHand, rightHand, leftLeg, rightLeg, timeToGet, position } = message
+    console.log(`Moving to position ${position}`)
     oscPort.send({
         address: "/posture",
         args: [
             {
                 type: "i",
-                value: leftHand
+                value: Math.floor(leftHand * 10)
             },
             {
                 type: "i",
-                value: rightHand
+                value: Math.floor(rightHand * 10)
             },
             {
                 type: "i",
-                value: leftLeg
+                value: Math.floor(leftLeg * 10)
             },
             {
                 type: "i",
-                value: rightLeg
+                value: Math.floor(rightLeg * 10)
             },
             {
                 type: "i",
-                value: timeToGet
+                value: parseInt(timeToGet) * 1000
             }
         ]
     });
